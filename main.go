5998ca338c2e8283735ffe120d17340cb5bffabc
Francisco Miamoto <fsmiamoto@gmail.com>
2019-12-01 10:27:50 -0300

    Creates temp and humidity functions with bindings

--- main.go
+++ main.go
@@ -1,27 +1,37 @@
 package main
 
 import (
-  "github.com/leaanthony/mewn"
-  "github.com/wailsapp/wails"
+	"math/rand"
+
+	"github.com/leaanthony/mewn"
+	"github.com/wailsapp/wails"
 )
 
-func basic() string {
-  return "World!"
+func getCurrentTemperature() float32 {
+	return 26.0 + 2*rand.Float32()
+}
+
+func getCurrentHumidity() float32 {
+	return 40 + 2*rand.Float32()
 }
 
 func main() {
 
-  js := mewn.String("./frontend/build/static/js/main.js")
-  css := mewn.String("./frontend/build/static/css/main.css")
+	js := mewn.String("./frontend/build/static/js/main.js")
+	css := mewn.String("./frontend/build/static/css/main.css")
 
-  app := wails.CreateApp(&wails.AppConfig{
-    Width:  1024,
-    Height: 768,
-    Title:  "niwa-app",
-    JS:     js,
-    CSS:    css,
-    Colour: "#131313",
-  })
-  app.Bind(basic)
-  app.Run()
+	app := wails.CreateApp(&wails.AppConfig{
+		Width:  1024,
+		Height: 768,
+		Title:  "niwa-app",
+		JS:     js,
+		CSS:    css,
+		Colour: "#131313",
+	})
+
+	// Bind functions for use in the frontend
+	app.Bind(getCurrentTemperature)
+	app.Bind(getCurrentHumidity)
+
+	app.Run()
 }
