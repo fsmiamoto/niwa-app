fb8f0605505a249fdc0fc5ad6e33066e4b2c3dcc
Francisco Miamoto <fsmiamoto@gmail.com>
2019-12-01 10:30:43 -0300

    Adds layout with the dashboard page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import logo from './logo.png';
-import './App.css';
-import HelloWorld from './components/HelloWorld';
+import React, { useState } from "react";
+import { Typography, Layout, Menu, Icon } from "antd";
+
+import Dashboard from "./pages/Dashboard";
+
+import "./App.css";
+import "antd/dist/antd.css";
+
+const { Content, Footer, Sider } = Layout;
+const { Title } = Typography;
 
 function App() {
+  const [menuCollapsed] = useState(false);
+
   return (
     <div id="app" className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to your new <code>wails/react</code> project.
-        </p>
-
-        <HelloWorld />
-      </header>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider collapsed={menuCollapsed}>
+          <div className="logo">
+            <Title
+              level={1}
+              style={{ color: "white", marginTop: 15, textAlign: "center" }}
+            >
+              Niwa
+            </Title>
+          </div>
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="dashboard" />
+              <span>Dashboard</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Content style={{ margin: "0 16px", marginTop: "10px" }}>
+            <Dashboard />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            UTFPR - Microcontroladores 2019
+          </Footer>
+        </Layout>
+      </Layout>
     </div>
   );
 }
