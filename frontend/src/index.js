1c86bea559e5a68968930eb57bfaf7d42eb527d6
Francisco Miamoto <fsmiamoto@gmail.com>
2019-12-01 10:31:26 -0300

    Changes index.js code style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'core-js/stable';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "core-js/stable";
+import "./index.css";
+import App from "./App";
 
-import * as Wails from '@wailsapp/runtime';
+import Wails from "@wailsapp/runtime";
 
 Wails.Init(() => {
-  ReactDOM.render(<App />, document.getElementById('app'));
+  ReactDOM.render(<App />, document.getElementById("app"));
 });
