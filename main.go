e4e9312fdc7ff6677bc656e749208de11262498f
Francisco Miamoto <fsmiamoto@gmail.com>
2019-12-06 07:55:51 -0300

    Creates methods for serial input

--- main.go
+++ main.go
@@ -1,21 +1,99 @@
 package main
 
 import (
-	"math/rand"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 
 	"github.com/leaanthony/mewn"
+	"github.com/tarm/serial"
 	"github.com/wailsapp/wails"
 )
 
-func getCurrentTemperature() float32 {
-	return 26.0 + 2*rand.Float32()
+var serialPort *serial.Port
+
+func getCurrentTempHum() []float64 {
+	temp, hum, err := readTempHumFromSerial()
+
+	fmt.Printf("Temp: %.2f Hum: %.2f\n", temp, hum)
+
+	if err != nil {
+		log.Print(err)
+		return []float64{-255, -255}
+	}
+	return []float64{temp, hum}
 }
 
-func getCurrentHumidity() float32 {
-	return 40 + 2*rand.Float32()
+func readTempHumFromSerial() (float64, float64, error) {
+	bytes, err := readFromSerial()
+	if err != nil {
+		return 0.0, 0.0, err
+	}
+
+	s := strings.TrimSpace(string(bytes))
+	splited := strings.Split(s, ";")
+
+	if len(splited) != 2 {
+		return 0.0, 0.0, fmt.Errorf("Problem with splited length!")
+	}
+
+	temp, err := strconv.ParseFloat(strings.TrimLeft(splited[0], "\x00"), 32)
+	if err != nil {
+		return 0.0, 0.0, err
+	}
+
+	hum, err := strconv.ParseFloat(splited[1], 32)
+	if err != nil {
+		return 0.0, 0.0, err
+	}
+
+	return temp, hum, nil
+}
+
+func readFromSerial() ([]byte, error) {
+	buf := make([]byte, 1)
+	data := make([]byte, 32)
+
+	// Wait for the first slash
+	for {
+		_, err := serialPort.Read(buf)
+		if err != nil {
+			return nil, nil
+		}
+
+		if string(buf[0]) == "/" {
+			break
+		}
+	}
+
+	// Append bytes until a other slash is found
+	for {
+		_, err := serialPort.Read(buf)
+		if err != nil {
+			return nil, nil
+		}
+
+		if string(buf[0]) == "/" {
+			break
+		}
+
+		data = append(data, buf[0])
+	}
+
+	return data, nil
 }
 
 func main() {
+	var err error
+
+	c := &serial.Config{Name: "/dev/ttyACM0", Baud: 9600}
+
+	serialPort, err = serial.OpenPort(c)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	js := mewn.String("./frontend/build/static/js/main.js")
 	css := mewn.String("./frontend/build/static/css/main.css")
@@ -30,8 +108,7 @@ func main() {
 	})
 
 	// Bind functions for use in the frontend
-	app.Bind(getCurrentTemperature)
-	app.Bind(getCurrentHumidity)
-
+	app.Bind(getCurrentTempHum)
 	app.Run()
+
 }
